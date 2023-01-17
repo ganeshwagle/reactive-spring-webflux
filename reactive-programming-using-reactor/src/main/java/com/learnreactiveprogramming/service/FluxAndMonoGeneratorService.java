d5b7b9b7da99fee1ae18f134bf7ae9d198bbc6e6
ganeshwagle <ganeshwagle135@gmail.com>
2023-01-17 10:20:05 +0530

    writing test cases using stepverifier

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,5 +1,25 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
-    
+    public static void main(String[] args) {
+        GenerateFluxAndMono generateFluxAndMono = new GenerateFluxAndMono();
+        generateFluxAndMono.generateNamesFlux().subscribe(System.out::println);
+        generateFluxAndMono.generateNameMono().subscribe(ele -> System.out.println("mono -> " + ele));
+    }
+}
+
+class GenerateFluxAndMono{
+    Flux<String> generateNamesFlux(){
+        return Flux.just("ganesh", "mahesh", "rakesh", "rajesh");
+        //return Flux.fromIterable(List.of("ganesh", "mahesh", "rakesh", "rajesh"));
+    }
+
+    Mono<String> generateNameMono(){
+        return Mono.just("ganesh");
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,26 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    GenerateFluxAndMono generateFluxAndMono = new GenerateFluxAndMono();
+    @Test
+    void fluxTest() {
+        StepVerifier.create(generateFluxAndMono.generateNamesFlux())
+                .expectNext("ganesh", "mahesh", "rakesh", "rajesh")
+                .verifyComplete();
+
+        /*StepVerifier.create(generateFluxAndMono.generateNamesFlux())
+                .expectNextCount(4)
+                .verifyComplete();*/
+
+       /* StepVerifier.create(generateFluxAndMono.generateNamesFlux())
+                .expectNext("ganesh")
+                .expectNextCount(3)
+                .verifyComplete();*/
+    }
 }
